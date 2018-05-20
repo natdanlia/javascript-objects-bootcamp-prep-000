82ae23a208daf2ad6d4c1d7d27186a9f40531d58
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-05-20 17:26:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {zeritu: "athidibign"}
 
-function updatPlaylist(obj, key, value){
-  return obj[key] = value;
+function updatePlaylist(playlist, artistName, songTitle){
+  return playlist[key] = value;
 }
